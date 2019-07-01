77d1be67ad374a4f7cf6daea31923ec71e3ffbc9
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-06-30 21:28:59 -0400

    5 questions and answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// // create mulitple choice questions(should be an array)
+// // record score.
+
+
+// var questions = [why did the chicken cross the road?];
+
+// var score = 0;
+
+// for(var i=0; i< questions.length; i++){
+//     var response = window.prompt(questions[i].prompt)
+//     if(response == questions[i].answer){
+//         score++;
+//         alert("correct")
+//     }
+//     else{
+//         alert("WRONG");
+//     }
+// }
+
+
