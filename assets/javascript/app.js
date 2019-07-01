a1a725fde80a32046ec5c22ebee384a97fd8c848
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-06-30 23:40:19 -0400

    Display the number of questions answered correctly ???

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,7 +17,37 @@
 //     }
 // }
 
-
 function check(){
     alert("FINISHED");
+
+
+            var question1 = document.trivia.question1.value;
+            var question2 = document.trivia.question2.value;
+            var question3 = document.trivia.question3.value;
+            var question4 = document.trivia.question4.value;
+            var question5 = document.trivia.question5.value;
+            var correct = 0;
+
+            if (question1 == "Philadelphia"){
+                correct++;
+            }
+            if(question2 == "His Mother"){
+                correct++;
+            }
+            if(question3 == "Bel-Air"){
+                correct++;
+            }
+            if(question4 == "Jazz"){
+                correct++;
+            }
+            if(question5 == "Cousin Carlton"){
+                correct++;
+            }
+
+
+    document.getElementById("afterSubmit").style.visibility = "visible";
+    document.getElementById("numberCorrect").innerHTML = "You Got " + correct + "Correct!";
+
 }
+
+
